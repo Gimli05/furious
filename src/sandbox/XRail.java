3deb76d560b3a24b8f1c4709cda0fe1cb8a3f70c
Topcsi <feher.peter94@gmail.com>
2017-03-25 00:27:11 +0100

    Javított xrail karakterkódolás

--- src/sandbox/XRail.java
+++ src/sandbox/XRail.java
@@ -8,32 +8,32 @@ public class XRail extends Rail{
 	
 	/**
 	 * A vonat a visitje során meghívja ezt a függvényt, mely a vonat elõzõ pozíciója alapján egyértelmûen megadja melyik sínre kell tovább mennie. 
-	 * Ezt a vonat eltárolja, és kõvetkezõ kõrben oda fog majd lépni.
+	 * Ezt a vonat eltárolja, és következõ körben oda fog majd lépni.
 	 * 
 	 * Az XRail az elsõ kettõ és a második kettõ sint kapcsolja pára, ezáltal megengedi hogy két irányba
 	 * haladjanak rajtam, mig figyeli hogy csak egy vonatelem lehessen rajta.
-	 * Az kõvetkezü sín visszaadása is ezen párok alapján tõrténik.
+	 * Az következõ sín visszaadása is ezen párok alapján történik.
 	 * 
 	 * @param trainPreviousRail	Az elõzõ sín, ahol a vonat állt.
-	 * @return	A kõvetkezõ sín ahova lépni fog a vonat.
+	 * @return	A következõ sín ahova lépni fog a vonat.
 	 */
 	@Override
 	public Rail getNextRail(Rail trainPreviousRail){
 		System.out.println("Class: XRail\t\t Object: "+this+"\t\t\t Method: getNextRail\t Param: "+trainPreviousRail+"\t A kovetkezo sin."); /* Kiíratás a Szkeleton vezérlésének */
 		
 		int idx=0; 											/*Index jegyzõ segédváltozó*/
-		while(neighbourRails.get(idx)!=trainPreviousRail){ 	/*Megkeressük a bejõvõ Rail-t*/
+		while(neighbourRails.get(idx)!=trainPreviousRail){ 	/*Megkeressük a bejövõ Rail-t*/
 			idx++;							 				/*Megkeressük hogy hányadik indexen van*/
 		}
 		
 		/**
-		 * Ezen a ponton érdemes belegondolni hogy hogy is mÅ±kõdik.
+		 * Ezen a ponton érdemes belegondolni hogy hogy is mûködik.
 		 * Mivel a sinek párokba vannak rendezve ezért a lehetséges párok:
 		 * 01,23,45,... indexek
 		 * 
 		 * Ezáltal ha az "idx" páratlan, akkor az elõtte lévõt kell visszadunk,
 		 * ha páros akkor az utána lévõt.
-		 *(Ezáltal akár tõbbes elágazás is építhetõ lenne)
+		 *(Ezáltal akár többes elágazás is építhetõ lenne)
 		 * 
 		 * Ezt is fogjuk tenni.
 		 */
